2504ff3e7a21fe5fad7cb97af33fbb22ac41b49d
Michael J. Gilliland <mjg.py3@gmail.com>
2013-03-28 18:28:40 -0400

    First commit, blank rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TicTacToe::Application.config.secret_token = 'f6cfb84f24f47a34b52c53caa8fa7d405ac4defe2110ce0b60ee87e87c81e8cdf7753c63eb5c532ffd6a946b4752b9645f712bee92f0d2a76897fe74b232f715'
