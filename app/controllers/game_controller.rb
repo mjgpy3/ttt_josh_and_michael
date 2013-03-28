1c307afe7477b8286aff4f7a00adfcfc159b6baa
Michael J. Gilliland <mjg.py3@gmail.com>
2013-03-28 19:14:18 -0400

    Added some stuff

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -1,9 +1,14 @@
 class GameController < ApplicationController
-
   before_filter :authenticate_user!
 
   def index
-    @users = User.all
+    @users = []
+
+    (User.all).each do |u|
+      if u.id != current_user.id
+        @users << u
+      end
+    end
   end
 
   def play
